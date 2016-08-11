aaaf9999fbde7afb3d83797b0566e71e02ab1e8a
ilanshom <ilanshom@gmail.com>
2016-08-10 22:05:38 -0700

    fixed indentation

--- download_wrapper.py
+++ download_wrapper.py
@@ -12,21 +12,21 @@ cmd1 = 'rm ./data/'
 path_to_licence = sys.argv[1]
 
 try:
-
+	
 	for fl in file_list:
-	    cmdf = cmd1+fl+'/*'
-	    print cmdf
-	    cmd = 'python download_NCTC_pipeline.py '+fl+' '+ path_to_licence
-	    print cmd
-	    os.system(cmdf)
-	    os.system(cmd)
+		cmdf = cmd1+fl+'/*'
+		print cmdf
+		cmd = 'python download_NCTC_pipeline.py '+fl+' '+ path_to_licence
+		print cmd
+		os.system(cmdf)
+		os.system(cmd)
 
 except: # file_list has a single name 
 
 	fl = str(file_list)
 	cmdf = cmd1+fl+'/*'
-    print cmdf
-    cmd = 'python download_NCTC_pipeline.py '+fl+' '+ path_to_licence
-    print cmd
-    os.system(cmdf)
-    os.system(cmd)
+	print cmdf
+	cmd = 'python download_NCTC_pipeline.py '+fl+' '+ path_to_licence
+	print cmd
+	os.system(cmdf)
+	os.system(cmd)
