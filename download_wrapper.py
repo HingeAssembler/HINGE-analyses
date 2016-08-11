6c00c01d4f7f05dfbac5101c9ee438b417091598
Ilan Shomorony <ilanshom@gmail.com>
2016-08-10 20:37:34 -0700

    fixed bug for single-item name list

--- download_wrapper.py
+++ download_wrapper.py
@@ -11,8 +11,20 @@ cmd1 = 'rm ./data/'
 
 path_to_licence = sys.argv[1]
 
-for fl in file_list:
-    cmdf = cmd1+fl+'/*'
+try:
+
+	for fl in file_list:
+	    cmdf = cmd1+fl+'/*'
+	    print cmdf
+	    cmd = 'python download_NCTC_pipeline.py '+fl+' '+ path_to_licence
+	    print cmd
+	    os.system(cmdf)
+	    os.system(cmd)
+
+except: # file_list has a single name 
+
+	fl = str(file_list)
+	cmdf = cmd1+fl+'/*'
     print cmdf
     cmd = 'python download_NCTC_pipeline.py '+fl+' '+ path_to_licence
     print cmd
